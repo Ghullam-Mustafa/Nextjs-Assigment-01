0a1e9f59b4360c5b07bf0d13b0148712a67998de
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-08-13 02:05:57 +0500

    how it works

--- src/app/components/howItWorks/howItWorksCards/HowItWorksCards.js
+++ src/app/components/howItWorks/howItWorksCards/HowItWorksCards.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import '../howItWorksCards/HowItWorksCards.css'
 
 export default function HowItWorksCards(props) {
@@ -6,7 +7,7 @@ export default function HowItWorksCards(props) {
     <div>
         <div className=" rounded-5 border-0 bg-transparent">
                 <div class="card cards bg-transparent border-0 " style={{width:'22rem'}} >
-                    <img src={props.src} class="card-img-top" className='img-fluid' alt="..." />
+                    <Image src={props.src} class="card-img-top" width={250} height={250} className='img-fluid' alt="..." />
                     <div class="card-body">
                         <h3 class="card-text text-center text-white px-1">{props.title}</h3>
                         <p className='text-center text-white '>{props.para}</p>
