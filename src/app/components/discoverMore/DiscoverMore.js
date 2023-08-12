187fd8be7b794edf4ab5b32ee933701cb7c5c05d
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-08-12 12:16:50 +0500

    some try

---
+++ src/app/components/discoverMore/DiscoverMore.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../discoverMore/DiscoverMore.css'
+import Image from 'next/image'
+import DiscoverCard from './discoverCard/DiscoverCard'
+// import img11 from '../../assets/Image Placeholder (2).png'
+// import img12 from '../../assets/Artist Avatar & Name.png'
+// import img21 from '../../assets/Image Placeholder (3).png'
+// import img22 from '../../assets/Artist Avatar & Name (1).png'
+// import img31 from '../../assets/Image Placeholder (4).png'
+// import img32 from '../../assets/Artist Avatar & Name (2).png'
+
+export default function DiscoverMore() {
+    return (
+        <div className='discover p-3'>
+            <div className="container ">
+                <div className="row">
+                    <div className="col-lg-9 col-md-9 col-sm-12">
+                        <p className=' text-white h5 display-5 fw-bold '>Discover More NFTs</p>
+                        <p className='text-white fs-4 font-monospace'>Explore new trending NFTs.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="container">
+                <div className="row ">
+                    <div className="col-lg-4 col-md-6 col-sm-12 my-4">
+                        <DiscoverCard  name="Distant Glaxy" />
+                    </div>
+                    {/* <div className="col-lg-4 col-md-6 col-sm-12 my-4">
+                        <DiscoverCard src={img21} name="Life of Edena" img2={img22} />
+                    </div> */}
+                    {/* <div className="col-lg-4 col-md-6 col-sm-12 my-4">
+                        <DiscoverCard src={img31} name="AstroFiction" img2={img32} />
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from '../app/components/navbar/Navbar';
 import HeroSection from '../app/components/heroSection/HeroSection';
 import TrendingCollection from '../app/components/trendingCollection/TrendingCollection';
-import DiscoverMore from '../components/discoverMore/DiscoverMore'
+import DiscoverMore from '../app/components/discoverMore/DiscoverMore'
 import FluidImg from '../app/components/fluidImg/FluidImg';
 import HowItWorks from '../app/components/howItWorks/HowItWorks';
 import Subscribe from '../app/components/subscribe/Subscribe';
