aaa60c3a59981d1f988d9efbaf11b7d417d7ea7f
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-08-20 22:16:40 +0500

    ww

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       <TrendingCollection />
       <TopCreater/>
       <BrowsCategories/>
-      {/* <DiscoverMore/> */}
+      <DiscoverMore/>
       <FluidImg/>
       <HowItWorks/>
       <Subscribe/>
